1086779a6ce61a3c81208a725acbd39f211666b2
Ashley Richmond <ashley@richmond.cool>
2024-04-14 16:16:36 +1000

    Removed invalid tags blocks.

--- src/network.tf
+++ src/network.tf
@@ -65,20 +65,12 @@ resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public)
   route_table_id = aws_route_table.public.id
   subnet_id = aws_subnet.public[count.index].id
-
-  tags = {
-    Name = "${var.project_name}-public"
-  }
 }
 
 resource "aws_route_table_association" "private" {
   count = length(aws_subnet.private)
   route_table_id = aws_route_table.private.id
   subnet_id = aws_subnet.private[count.index].id
-
-  tags = {
-    Name = "${var.project_name}-private"
-  }
 }
 
 resource "aws_route53_zone" "internal" {
